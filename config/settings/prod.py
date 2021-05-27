8d8f3c10775dd85895f69bfa082f07468361b973
박서왕 <sking147@naver.com>
2021-05-27 15:44:16 +0900

    db connect

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,3 +4,13 @@ ALLOWED_HOSTS = ['13.124.229.142']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sbclc',
+        'USER': 'dbmasteruser',
+        'PASSWORD': '98,RC_xKQ$}&M8R-SRrtb>+2}SMy8WsJ',
+        'HOST': 'ls-2e3ea7b61c9294d9ec721d280147ece70acd1208.calkbqcnylxq.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '5432',
+    }
+}
