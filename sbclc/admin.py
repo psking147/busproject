0cc338588e8075e4d2e19eab29bd1a137c0a3202
박서왕 <sking147@naver.com>
2021-06-03 01:19:49 +0900

    debug

--- sbclc/admin.py
+++ sbclc/admin.py
@@ -12,7 +12,7 @@ class LineAdmin(admin.ModelAdmin):
 
 
 class StopCongestionAdmin(admin.ModelAdmin):
-    search_fields = ['stop.ars', 'stop.name']
+    search_fields = ['stop__ars', 'stop__name']
 
 
 class LineCongestionAdmin(admin.ModelAdmin):
